b897011b474379ee49d22f6484143fb5accc8260
Nihal <nsnihalshah@gmail.com>
2016-04-12 00:21:24 -0500

    Merge works

--- static/data.js
+++ static/data.js
@@ -14,12 +14,41 @@ function submitTime(day){
             "day":day
         },
         success:function(data){
-            console.log(data["images"][0]);
+            create_images(data["images"], day)
         }
     });
 
 }
 
+
+
+function create_images(image_list, day){
+
+    image1 = "/static/images/"+String(day)+"/"+String(image_list[0]);
+    image2 = "/static/images/"+String(day)+"/"+String(image_list[1]);
+    image3 = "/static/images/"+String(day)+"/"+String(image_list[2]);
+    image4 = "/static/images/"+String(day)+"/"+String(image_list[3]);
+    console.log(image1);
+    resemble.outputSettings({
+      errorColor: {
+          red: 255,
+          green: 0,
+          blue: 255
+      },
+      errorType: 'movement',
+      transparency: 1.0,
+      largeImageThreshold: 1200
+    });
+    var merge_image = new Image();
+    var merge = function(data){
+		merge_image.src = data.getImageDataUrl();
+		$('#image').html(merge_image);
+
+    };
+    var ret = resemble(image1).compareTo(image2).ignoreAntialiasing().onComplete(merge);
+
+
+}
 function submitDay(day){
     window.location.href = "http://localhost:5000/"+String(day)+"/";
 }
